47ab66b2cb9a5559d937a35862812241ca7e9d5c
NihadRasulzada <nihadrs2005@gmail.com>
2024-11-21 18:31:05 +0300

    .

--- MultiShop.Catalog.Api/Program.cs
+++ MultiShop.Catalog.Api/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
-using MultiShop.Catalog.Api.Configurations;
-using MultiShop.Catalog.Api.Extensions;
 using MultiShop.Catalog.Api.Services.AboutServices;
 using MultiShop.Catalog.Api.Services.BrandServices;
 using MultiShop.Catalog.Api.Services.CategoryServices;
