4d3e288e4cfcae415a28723cfa1fd0ceab761cbc
NihadRasulzada <nihadrs2005@gmail.com>
2024-11-21 18:29:48 +0300

    CategoryNotAuthorize

--- MultiShop.Catalog.Api/Program.cs
+++ MultiShop.Catalog.Api/Program.cs
@@ -18,9 +18,6 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-TimeZoneInfo azTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Azerbaijan Standard Time");
-DateTime currentTimeInAZT = TimeZoneInfo.ConvertTime(DateTime.UtcNow, azTimeZone);
-
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
 {
     opt.Authority = builder.Configuration["IdentityServerUrl"];
