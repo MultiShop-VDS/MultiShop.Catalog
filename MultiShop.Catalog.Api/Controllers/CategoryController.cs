748ce04247605f044d48041577a4dd66a75d3465
NihadRasulzada <nihadrs2005@gmail.com>
2024-11-22 00:15:59 +0400

    ...

--- MultiShop.Catalog.Api/Controllers/CategoryController.cs
+++ MultiShop.Catalog.Api/Controllers/CategoryController.cs
@@ -5,7 +5,7 @@ using MultiShop.Catalog.Api.Services.CategoryServices;
 
 namespace MultiShop.Catalog.Api.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CategoryController : ControllerBase
